0b051c125d60cf8fab468b6c18967cb7d68da4c4
rhthorat <Pravin@LAPTOP-1FP771V6>
2021-09-07 20:51:29 -0400

    Rujuta Code

--- group/Program.cs
+++ group/Program.cs
@@ -9,6 +9,7 @@ namespace group
             Console.WriteLine("Hello World from Riya!");
             Console.WriteLine("Hello World from Himanshu!");
             Console.WriteLine("Hi this is Himanshu Again!");
+            Console.WriteLine("Hi this is Rujuta");
         }
     }
 }
